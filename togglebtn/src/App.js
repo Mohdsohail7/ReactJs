a9483e154a48666ac0d8121da960c3a165aa65c7
MohdSohail7 <sohailmohd437@gmail.com>
2023-12-26 18:25:10 +0530

    Toggle Button Task Completed

--- togglebtn/src/App.js
+++ togglebtn/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toggleBtnText, setToggleBtnText] = useState(false);
+
+  const toggleBtnHandler = () => {
+    console.log("Clicked")
+    setToggleBtnText(!toggleBtnText)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type='button' onClick={toggleBtnHandler} >
+          {
+            toggleBtnText ? "ON" : "OFF"
+          }
+        </button>
       </header>
     </div>
   );
