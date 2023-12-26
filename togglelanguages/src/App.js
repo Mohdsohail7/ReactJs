6eca2a16fa6a18bb06fcabf38eb50fb6868aba56
MohdSohail7 <sohailmohd437@gmail.com>
2023-12-26 19:21:57 +0530

    completed

--- togglelanguages/src/App.js
+++ togglelanguages/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
+import React, { createContext, useState, useContext } from 'react';
 import './App.css';
 
+
+
+const languages = ["JavaScript", "Python", "java", "C++", "C#", "Dot Net"];
+  const LanguageContext = createContext({
+    languages,
+    language: languages[0],
+    setLanguage: () => {},
+  })
+
 function App() {
+  const [language, setLanguage] = useState(languages[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LanguageContext.Provider value={{languages, language, setLanguage}} >
+        <MainSection />
+      </LanguageContext.Provider>
     </div>
+  );
+}
+
+function MainSection(){
+  const {languages, language, setLanguage} = useContext(LanguageContext);
+  const currentIndex = languages.indexOf(language);
+
+  const toggleLanguage = () => {
+      if(currentIndex === languages.length-1){
+          setLanguage(languages[0]);
+      }else{
+          setLanguage(languages[currentIndex+1]);
+      }
+  }
+
+
+  return(
+      <div>
+          <p id='favoriteLanguage'>{`Favorite Programming Language: ${language}`}</p>
+          <button id='changeFavorite' onClick={toggleLanguage}>
+              Change language
+          </button>
+      </div>
   );
 }
 
